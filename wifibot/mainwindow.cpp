f462220f860e7da363ce91c787a3110dc2a3830a
Nathan Haudot <nathan.haudot@outlook.fr>
2021-06-07 14:32:53 +0200

    UI update

--- wifibot/mainwindow.cpp
+++ wifibot/mainwindow.cpp
@@ -1,11 +1,35 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+#include <QRegExpValidator>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // RegEx port
+    ui->lineEdit_Port->setValidator(new QRegExpValidator(QRegExp("[0-9]{5}")));
+
+    // RegEx IP
+    QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
+    QRegExp ipRegex ("^" + ipRange
+                     + "\\." + ipRange
+                     + "\\." + ipRange
+                     + "\\." + ipRange + "$");
+    QRegExpValidator *ipValidator = new QRegExpValidator(ipRegex, this);
+
+    ui->lineEdit_IP->setValidator(ipValidator);
+
+
+
+
+
+
+
+
+
     //wifibotv3.doConnect();
 }
 
